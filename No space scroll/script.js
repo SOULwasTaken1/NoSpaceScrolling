a76a0f309c41d3727bf61d1916d5e897c2fcce76
VOID SOUL <80249387+SOULwasTaken1@users.noreply.github.com>
2023-05-06 15:15:08 +0600

    Update script.js

--- No space scroll/script.js
+++ No space scroll/script.js
@@ -1,5 +1,32 @@
-window.addEventListener('keydown', function(e) {
-    if(e.keyCode == 32 && e.target == document.body) {
-      e.preventDefault();
-    }
-  });
+setInterval(function() {(function() {
+  var k = function(action){
+  var eventObj = document.createEvent("Events");
+  
+  eventObj.initEvent("keydown", true, true);
+  eventObj.keyCode = 75;
+  eventObj.which = 75;
+  
+  document.body.dispatchEvent(eventObj);
+  };
+  
+  var killSpaceBar = function(evt) {
+  
+  var target = evt.target || {},
+  isInput = ("INPUT" == target.tagName || "TEXTAREA" == target.tagName || "SELECT" == target.tagName || "EMBED" == target.tagName);
+  
+  // if we’re an input or not a real target exit
+  if(isInput || !target.tagName) return;
+  
+  // if we’re a fake input like the comments exit
+  if(target && target.getAttribute && target.getAttribute('role') === 'textbox') return;
+  
+  // ignore the space and send a ‘k’ to pause
+  if (evt.keyCode === 32) {
+  evt.preventDefault();
+  k();
+  }
+  };
+  
+  document.addEventListener("keydown", killSpaceBar, false);
+  console.log("Executed Kill Space scroll")
+  })();}, 1000)
